a9719d20e8ff3d710f748f590fc62414d278b0cb
Sujeet Madihalli <sujeet@example.com>
2026-04-14 15:43:35 -0400

    Add build_files.sh for Vercel static files collection

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Run migrations (Optional, but good practice if you want automatic migrations)
+python3 manage.py migrate --noinput
+
+# Collect static files
+python3 manage.py collectstatic --noinput
