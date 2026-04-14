78a2749431253161372437ec935c6e5c00bea288
Sujeet Madihalli <sujeet@example.com>
2026-04-14 16:00:14 -0400

    Add missing manual migration for user relation on Expense model

---
+++ expenses/migrations/0002_expense_user.py
@@ -0,0 +1,21 @@
+# Generated manually to bypass local environment issues
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('expenses', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='expense',
+            name='user',
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            preserve_default=False,
+        ),
+    ]
